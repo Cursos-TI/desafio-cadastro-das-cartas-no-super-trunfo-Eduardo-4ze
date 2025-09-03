86b8d78c08ed15dcb930da32fab2f42145062637
Eduardo-4ze <eduardofoxsaze@gmail.com>
2025-09-03 15:51:14 +0000

    criei um jogo super trunfo com sistema de cartas onde são selecionadas e comparadas ,usando estados do brasil

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,47 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Super Trunfo - Cartas de Cidades Brasileiras
+// Este código cria duas cartas de cidades brasileiras
+// Cada carta possui: Estado, Código, Cidade, População, Área, PIB e Pontos Turísticos
+// Os dados são aproximados e apenas para fins do jogo
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Carta 1 - Porto Alegre
+    char estado1[] = "Rio Grande do Sul"; // Nome do estado
+    char codigo1[] = "RS01";             // Código da carta
+    char cidade1[] = "Porto Alegre";    // Nome da cidade
+    int populacao1 = 1485000;            // População aproximada
+    float area1 = 496.8;                 // Área em km2
+    float pib1 = 82.0;                   // PIB aproximado em bilhões
+    int pontos1 = 20;                     // Numero de pontos turísticos(estimado)
 
-    return 0;
+    // Carta 2 - são Paulo
+    char estado2[] = "São Paulo";
+    char codigo2[] = "SP01";
+    char cidade2[] = "São Paulo";
+    int populacao2 = 12325000;
+    float area2 = 1521.1;
+    float pib2 = 699.2;
+    int pontos2 = 50;
+
+    // Impressão das cartas
+    printf("\n--- Carta 1 ---\n");
+    printf("Estado: %s\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Cidade: %s\n", cidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km2\n", area1);
+    printf("PIB: %.2f bilhoes\n", pib1);
+    printf("Pontos turisticos: %d\n", pontos1);
+
+    printf("\n--- Carta 2 ---\n");
+    printf("Estado: %s\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km2\n", area2);
+    printf("PIB: %.2f bilhoes\n", pib2);
+    printf("Pontos turisticos: %d\n", pontos2);
+
+    return 0; // mostra que o programa terminou corretamente
 }
